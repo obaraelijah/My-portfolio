6103c677451cfdc93e03394d9eae9136b1e49ef5
elijah obara <elijahobara357@gmail.com>
2022-08-17 15:36:50 +0300

    added app functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-import './about.css'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+const form = useRef();
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_51my9qh', 'template_7xi2hmy', form.current, 'T_HTb5JpeIuOiSGBZ')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+
+    e.target.reset()
+};
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>elijahobara357@gmail.com</h5>
+            <a href="mailto:elijahobara357@gmail.com" target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+254703229589</h5>
+            <a href="https://api.whatsapp.com/send?phone+254703229589" target="_blank">Send a Message</a>
+          </article>
+
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name'  placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
+const data= [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Platform for Money Movements',
+    github: 'http://github.com',
+    demo: 'https://cdn.dribbble.com/userupload/3272863/file/original-cc9365b311e8ca8169cd4359f7593ffa.png?compress=1&resize=752x'
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: 'Axel Arigato Virtual Fitting App',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/19116575-Axel-Arigato-Virtual-Fitting-App'
+  },
+  {
+    id: 1,
+    image: IMG3,
+    title: 'Blocklink logo concept',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/19122368-Blocklink-logo-concept-pt-3'
+  },
+  {
+    id: 1,
+    image: IMG4,
+    title: 'Blocklink logo concept',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/19122368-Blocklink-logo-concept-pt-3'
+  },
+  {
+    id: 1,
+    image: IMG5,
+    title: 'Blocklink logo concept',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/19122368-Blocklink-logo-concept-pt-3'
+  },
+  {
+    id: 1,
+    image: IMG6,
+    title: 'Blocklink logo concept',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/19122368-Blocklink-logo-concept-pt-3'
+  },
+
+]
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}/>
+              </div>
+              <h3>{title}</h3>
+               <div className="portfolio__item-cta">
+               <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+               </div>
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
